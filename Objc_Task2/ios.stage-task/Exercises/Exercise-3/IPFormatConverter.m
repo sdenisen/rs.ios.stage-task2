480ab07674564a556953ba1981879979bf84f0d8
sdenisenko <sdenisen@ymail.com>
2021-05-17 00:18:52 +0300

    feat: implement IpConverter; unit tests passed;

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,32 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    if (numbersArray ==  nil || [numbersArray count] ==0){
+        return @"";
+    }
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"intValue < 0 "];
+    if ([[numbersArray filteredArrayUsingPredicate:predicate] count] > 0){
+        return @"Negative numbers are not valid for input.";
+    }
+    
+    NSPredicate *predicate2 = [NSPredicate predicateWithFormat:@"intValue > 255"];
+    if ([[numbersArray filteredArrayUsingPredicate:predicate2] count] > 0){
+        return @"The numbers in the input array can be in the range from 0 to 255.";
+    }
+    
+    NSArray *r;
+    if ([numbersArray count] <= 4)
+        r = numbersArray;
+    else
+        r = [numbersArray subarrayWithRange:NSMakeRange(0, 4)];
+    
+    NSMutableArray *rr = [[NSMutableArray alloc] initWithArray:r];
+    for (int i=(int)[rr count]; i<4; i++){
+        [rr addObject:@(0)];
+    }
+    return [rr componentsJoinedByString:@"."];
 }
 
 @end
