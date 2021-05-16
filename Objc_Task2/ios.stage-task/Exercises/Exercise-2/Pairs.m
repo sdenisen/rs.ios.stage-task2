98d1836960cd0d5aaf7bdce16710fe8139a155b2
sdenisenko <sdenisen@ymail.com>
2021-05-16 22:37:10 +0300

    feat: implement countPairs method; unit tests passed;

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+//    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"intValue == %d", [number intValue]];
+//    int a = [[array filteredArrayUsingPredicate: predicate] count];
+    NSInteger count = 0;
+    for(int i=0; i<[array count]; i++){
+        for (int j=i+1; j<[array count]; j++){
+            if (abs([array[i] intValue] - [array[j] intValue]) == [number intValue]){
+                count = count + 1;
+            }
+        }
+    }
+    
+    return count;
 }
 
 @end
