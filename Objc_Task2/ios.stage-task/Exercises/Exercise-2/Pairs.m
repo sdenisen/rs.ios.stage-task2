8bb09cdeb20e217e36dba5f951bf96ec9bdb4cf9
sdenisenko <sdenisen@ymail.com>
2021-05-17 00:27:48 +0300

    refactor: remove commented lines; without testing;

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,8 +3,6 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-//    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"intValue == %d", [number intValue]];
-//    int a = [[array filteredArrayUsingPredicate: predicate] count];
     NSInteger count = 0;
     for(int i=0; i<[array count]; i++){
         for (int j=i+1; j<[array count]; j++){
