236d634cc56d33682c3f641bc9da39426770f2c5
sdenisenko <sdenisen@ymail.com>
2021-05-17 08:06:02 +0300

    feat: cover some new cases and optimise code;

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -7,28 +7,29 @@
     if (numbersArray ==  nil || [numbersArray count] ==0){
         return @"";
     }
+
+    NSArray *four_oktets;
+    if (numbersArray.count <= 4){
+        NSMutableArray *tmp = [[NSMutableArray alloc] initWithArray: numbersArray];
+        for (int i=(int)tmp.count; i<4; i++){
+            [tmp addObject:@(0)];
+        }
+        four_oktets = [[NSArray alloc] initWithArray: tmp];
+    }
+    else
+        four_oktets = [[NSArray alloc] initWithArray: [numbersArray subarrayWithRange:NSMakeRange(0, 4)]];
     
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"intValue < 0 "];
-    if ([[numbersArray filteredArrayUsingPredicate:predicate] count] > 0){
+    if ([[four_oktets filteredArrayUsingPredicate:predicate] count] > 0){
         return @"Negative numbers are not valid for input.";
     }
     
     NSPredicate *predicate2 = [NSPredicate predicateWithFormat:@"intValue > 255"];
-    if ([[numbersArray filteredArrayUsingPredicate:predicate2] count] > 0){
+    if ([[four_oktets filteredArrayUsingPredicate:predicate2] count] > 0){
         return @"The numbers in the input array can be in the range from 0 to 255.";
     }
     
-    NSArray *r;
-    if ([numbersArray count] <= 4)
-        r = numbersArray;
-    else
-        r = [numbersArray subarrayWithRange:NSMakeRange(0, 4)];
-    
-    NSMutableArray *rr = [[NSMutableArray alloc] initWithArray:r];
-    for (int i=(int)[rr count]; i<4; i++){
-        [rr addObject:@(0)];
-    }
-    return [rr componentsJoinedByString:@"."];
+    return [four_oktets componentsJoinedByString:@"."];
 }
 
 @end
